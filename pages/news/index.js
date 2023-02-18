72ba24fcdf1b9472188ba1f794d05f1394b7a7f2
Alek Novkovski <alek@greatcode2.com>
2023-02-18 03:54:56 +0100

    Add a next.js link to a 'news story', shows how it works

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,19 @@
-export default function NewsPage() {
-    return <h1>My news page</h1>
+import Link from 'next/link';
+
+function NewsPage() {
+    return (
+        <>
+            <h1>My news page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/nextjs-is-a-great-framework'>
+                        NextJS Is A Great Framework
+                    </Link>
+                </li>
+                <li>Something Else</li>
+            </ul>
+        </>
+    );
 }
+
+export default NewsPage;
