3ba72f0980461bb61f172cd134628759ecdd7be1
Alek Novkovski <alek@greatcode2.com>
2023-02-20 00:01:41 +0100

    Replace dummy hardcoded meetup w one from DB

--- app2/pages/[meetupId]/index.js
+++ app2/pages/[meetupId]/index.js
@@ -1,14 +1,14 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 import {MongoClient, ObjectId} from "mongodb";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-      title='First Meetup'
-      address='Some Street 5, Some City'
-      description='This is a first meetup'
-    />
+      <MeetupDetail
+          image={props.meetupData.image}
+          title={props.meetupData.title}
+          address={props.meetupData.address}
+          description={props.meetupData.description}
+      />
   );
 }
 
@@ -39,7 +39,7 @@ export async function getStaticProps(context) {
   const meetupsCollection = db.collection('meetups');
 
   const selectedMeetup = await meetupsCollection.findOne({
-    _id: ObjectId(meetupId),
+    _id: new ObjectId(meetupId),
   });
 
   client.close();
