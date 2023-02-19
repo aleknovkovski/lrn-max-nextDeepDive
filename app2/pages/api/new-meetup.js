f24bd6091df8bc2de5a76bcb1e9fc3a5e2c91ce4
Alek Novkovski <alek@greatcode2.com>
2023-02-19 01:37:59 +0100

    Create new doc in db on new POST req

--- app2/pages/api/new-meetup.js
+++ app2/pages/api/new-meetup.js
@@ -1,8 +1,20 @@
+import { MongoClient } from 'mongodb';
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-    console.log(data)
-    res.status(200).send('the route works!' + JSON.stringify(data))
+
+    const client = await MongoClient.connect(process.env.MONGO_URI);
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
